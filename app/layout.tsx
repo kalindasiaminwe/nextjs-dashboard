8911b178a4a644b23b594bbdeb00bee5c177fbd2
Kalinda Siaminwe <ksiaminwe@gmail.com>
2025-02-28 12:12:35 +0200

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css'
+import {merriweather} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={ `${merriweather.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Merriweather, Lusitana } from 'next/font/google';
+
+export const merriweather = Merriweather({
+    subsets: ['latin'],
+    weight: ["400", "700"]
+
+});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ["400", "700"]
+
+});
